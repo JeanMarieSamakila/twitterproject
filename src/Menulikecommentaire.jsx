e2874d68a79a84c1f8cd347690f5ab625037bdc2
JeanMarieSAMAKILA <samakilambilikajeanmarie@gmail.com>
2024-01-17 18:30:55 +0200

    Incrementation de like

--- src/Menulikecommentaire.jsx
+++ src/Menulikecommentaire.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react"
+
 export default function Menulikecommentaire (){
+   const [compteur, setCompteur] = useState(0);
+  const [couleur, setCouleur] = useState('');
+
+
+   const [valeur, setValeur] = useState(0);
+
+   const gererIncrementationDecrementation = () => {
+     if (valeur !== 1) {
+       setValeur(valeur + 1);
+       setCouleur('blue');
+
+     } else {
+       setValeur(valeur - 1);
+       setCouleur('');
+     }
+   };
+
+    
+    const handleClick =  () => {
+        setCompteur(compteur + 1);
+    }
+   
     return(
         <>  
             <container className="p-2 flex space-x-5">
-                <img src="images/Reply.svg" alt="" />
-                <p className="text-white">57</p>
-                <img src="images/Retweet.svg" alt="" />
-                <p className="text-white">144</p>
-                <img src="images/React.svg" alt="" />
-                <p className="text-white">184</p>
-                <img src="images/Share.svg" alt="" />
+                <img onClick={gererIncrementationDecrementation}  src="images/Reply.svg" alt="" />
+                <p className="text-white">0</p>
+                <img onClick={handleClick} src="images/Retweet.svg" alt="" />
+                <p className="text-white">{compteur}</p>
+                <img onClick={gererIncrementationDecrementation} style={{ backgroundColor: couleur, borderRadius: '50%', padding: '5px' }} src="images/React.svg" alt=""/>
+                <p className="text-white">{valeur}</p>
+                <img  src="images/Share.svg" alt="" />
             </container>
         </>
     )
--- src/Tweetchild.jsx
+++ src/Tweetchild.jsx
@@ -1,7 +1,6 @@
 export default function Tweetchild(){
     return(
-        <>
-            <div id="btntweet" className='p-2 flex space-x-60 justify-between'>
+            <section id="tweet" className="flex justify-between">
                 
                     <div className="flex space-x-5">
                         <img src="images/Media.svg" alt="" />
@@ -10,10 +9,7 @@ export default function Tweetchild(){
                         <img src="images/Emoji.svg" alt="" />
                         <img src="images/Schedule.svg" alt="" />
                     </div>
-                    <div className="btntweet">
-                        <button id="btn" className='bg-blue-500 rounded-3xl h-12 w-28'>Tweet</button>
-                    </div>
-            </div>
-        </>
+                    <button id="btn" className='bg-blue-500 rounded-3xl h-12 w-28'>Tweet</button>
+            </section>
     )
 }
